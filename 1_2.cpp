54f1bd2ab281c363f17ae55a98c2ba2887a1b5fc
ShilovGrig <101323985+ShilovGrig@users.noreply.github.com>
2022-12-26 18:01:33 +0300

    Add files via upload

---
+++ 1_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 150;
+    float b = 15.933;
+    short c = 250;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    int day = 17, year = 2000;
+    string month = "июнь";
+    cout << "Моя дата рождения: " << day << month << year << "года."<< endl;
+    const float x = 2.3;
+    const string y = "WINDOWS";
+    cout << x << " " << y << endl;
+    return 0;
+}
